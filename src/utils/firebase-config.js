38b387d90eea20499c799693cb2e9f586d730462
Mahima Devi <mahidv5687@gmail.com>
2024-06-03 19:31:43 +0530

    second

--- src/utils/firebase-config.js
+++ src/utils/firebase-config.js
@@ -1,7 +1,9 @@
+require('dotenv').config();
+
 import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth"; 
 const firebaseConfig = {
-  apiKey: "AIzaSyD7f0Pmgi5a82sa-7TWdhOGO1gN19_VQRQ",
+  apiKey: process.env.apiKey,
   authDomain: "movie-library-969bd.firebaseapp.com",
   projectId: "movie-library-969bd",
   storageBucket: "movie-library-969bd.appspot.com",
